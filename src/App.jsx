ccffae955d6a0ca8255146a63d34ed65610e3d69
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-07-27 20:53:22 +0500

    unique

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,18 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your 🥵 trip</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20}, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add </button>
+    </form>
   );
 }
 
@@ -32,8 +41,8 @@ function PackingList() {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} />
+        {initialItems.map((unique) => (
+          <Item item={unique} key={unique.id}/>
         ))}
       </ul>
     </div>
