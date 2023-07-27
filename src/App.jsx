af01ff9f80d3c57862304e7a7d059a3e36aae5d8
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-07-27 15:34:53 +0500

    basic-layout

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+export default function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+
+  )
+}
+
+function Logo() {
+  return <h1>🌴Far Away💼</h1>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 🥵 trip</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">LIST</div>
+  );
+}
+
+function Stats() {
+  return(
+    <footer className="stats">
+      <em>👦 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
